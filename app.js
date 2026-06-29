257b7e94fb9caab9997be0e0ca7f58f2d524f11e
Sariful Ali <sksarifulali129@gmail.com>
2026-06-29 07:35:46 +0530

    learned about routes , routes parameters in express

--- app.js
+++ app.js
@@ -1,2 +1,38 @@
-console.log("hello world , howare you, nodemon");
-console.log("testing nodemon");
+/*console.log("hello world , howare you, nodemon");
+console.log("testing nodemon");*/
+
+const express = require("express");
+const app = express();
+app.get("/", (req,res) => {
+    res.send("hello") 
+}
+)
+
+app.get("/about", (req,res) => {
+    res.send("Welcome to About Page")
+}
+)
+
+app.get("/contact", (req,res) => {
+    res.send("Welcome to Contact Page")
+}
+)
+app.get("/admin", (req,res) => {
+    res.send("Welcome to Admin Page")
+})
+app.get("/student", (req,res) => {
+    res.send("Welcome to student page")
+})
+app.get ("/books/:id", (req,res) => {
+    res.send(req.params.id);
+});
+app.get("/users/:name", (req,res) => {
+    res.send("Hello" + req.params.name);
+});
+app.get("/developers/:category/:id", (req,res) => {
+    res.send("Hello developer"  + req.params.category + ".Your id is: "  + req.params.id);
+});
+app.get("/ebooks/:id/:category/:name" , (req,res) => {
+    res.send(req.params.id + req.params.category + req.params.name);
+});
+app.listen(3000); 
