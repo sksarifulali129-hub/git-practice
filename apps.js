4286b75f2353af6398d86a87f579acaf5ec29cf6
Sariful Ali <sksarifulali129@gmail.com>
2026-06-30 09:25:42 +0530

    large json data uploaded to mongodb atlas using express(nodejs) , the data was a file downloaded from a third party site

---
+++ apps.js
@@ -0,0 +1,32 @@
+const { MongoClient } = require("mongodb");
+const fs = require("fs");
+
+const uri = "mongodb://sksarifulali129_db_user:Mon85%40%40**@ac-yakuung-shard-00-00.u8hrk8d.mongodb.net:27017,ac-yakuung-shard-00-01.u8hrk8d.mongodb.net:27017,ac-yakuung-shard-00-02.u8hrk8d.mongodb.net:27017/?ssl=true&replicaSet=atlas-pfmfuu-shard-0&authSource=admin&appName=Cluster0";
+
+const client = new MongoClient(uri);
+
+async function uploadJSON() {
+    try {
+        await client.connect();
+
+        const db = client.db("mockaro");
+        const collection = db.collection("user");
+
+        // Read JSON file
+        const jsonData = JSON.parse(
+            fs.readFileSync("mockaroo.json", "utf8")
+        );
+
+        // Insert data
+        await collection.insertMany(jsonData);
+
+        console.log("JSON uploaded successfully!");
+    } catch (err) {
+        console.error(err);
+    } finally {
+        await client.close();
+    }
+}
+
+uploadJSON();
+
